6e1bd3428bc3884925a46b43bc0b8d55e286b751
juna0613 <yuta.naito@gmail.com>
2016-04-25 22:14:00 +0900

    add AAD

--- src/Autod/Core/Aad.cs
+++ src/Autod/Core/Aad.cs
@@ -6,7 +6,115 @@ using System.Threading.Tasks;
 
 namespace Autod.Core
 {
-    class Aad
+    public class Aad
     {
+		public int Index { get; private set; }
+		public double Value { get; private set; }
+
+		public Aad(double value)
+		{
+			Value = value;
+			Index = AadCalculationStack.Instance.NewIndex ();
+		}
+
+		public double Derivative(Aad variable)
+		{
+			var stack = AadCalculationStack.Instance;
+			var data = stack.Data.ToArray();
+			var vals = new List<double> (Index + 1);
+			vals [Index] = 1.0;
+			for (var i = 1; i <= data.Length; ++i) 
+			{
+				var derivData = data [data.Length - i];
+				if (derivData.DerivTarget <= Index) 
+				{
+					vals [derivData.DerivBy] += derivData.Value * vals [derivData.DerivTarget];
+				}
+			}
+			return vals [variable.Index];
+		}
+
+		public static Aad operator+(Aad left, Aad right)
+		{
+			var res = new Aad (left.Value + right.Value);
+			AadCalculationStack.Instance.Add (left.Index, res.Index, 1.0);
+			AadCalculationStack.Instance.Add (right.Index, res.Index, 1.0);
+			return res;
+		}
+
+		public static Aad operator-(Aad left, Aad right)
+		{
+			var res = new Aad (left.Value - right.Value);
+			AadCalculationStack.Instance.Add (left.Index, res.Index, 1.0);
+			AadCalculationStack.Instance.Add (right.Index, res.Index, -1.0);
+			return res;
+		}
+
+		public static Aad operator*(Aad left, Aad right)
+		{
+			var res = new Aad (left.Value * right.Value);
+			AadCalculationStack.Instance.Add (left.Index, res.Index, right.Value);
+			AadCalculationStack.Instance.Add (right.Index, res.Index, left.Value);
+			return res;
+		}
+
+		public static Aad operator/(Aad left, Aad right)
+		{
+			var res = new Aad (left.Value / right.Value);
+			AadCalculationStack.Instance.Add (left.Index, res.Index, 1.0 / right.Value);
+			AadCalculationStack.Instance.Add (right.Index, res.Index, -left.Value / right.Value / right.Value);
+			return res;
+		}
+
+		public static implicit operator Aad(double x)
+		{
+			return new Aad (x);
+		}
+
+		public static implicit operator Aad(int x)
+		{
+			return new Aad (x);
+		}
+
+		public static implicit operator Aad (float x)
+		{
+			return new Aad (x);
+		}
     }
+
+	public class AadCalculationStack
+	{
+		private int _numOfVariables = 0;
+		static AadCalculationStack()
+		{
+			Instance = new AadCalculationStack();
+		}
+		public static AadCalculationStack Instance {
+			get;
+			private set;
+		}
+		public int NewIndex()
+		{
+			return _numOfVariables++;
+		}
+		private List<AadDerivData> _values = new List<AadDerivData>();
+		public void Add(int derivByIndex, int derivTargetIndex, double value)
+		{
+			_values.Add (new AadDerivData (derivByIndex, derivTargetIndex, value));
+		}
+		public IEnumerable<AadDerivData> Data { get { return _values; } }
+	}
+
+	public class AadDerivData
+	{
+		public int DerivBy { get; private set; }
+		public int DerivTarget { get; private set; }
+		public double Value { get ; private set; }
+		public AadDerivData(int derivBy, int derivTarget, double value)
+		{
+			DerivBy = derivBy;
+			DerivTarget = derivTarget;
+			Value = value;
+		}
+	}
 }
---
+++ tests/AutodTest/AadTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using Autod.Core;
+namespace AutodTest
+{
+	[TestFixture]
+	public class AadTest
+	{
+		[Test]
+		public void AadTest1 ()
+		{
+			var x0 = new Aad (4.0);
+			var x1 = new Aad (2.0);
+			var y = F (x0, x1);
+			Assert.That (y.Value, Is.EqualTo (66));
+			Assert.That (y.Derivative (x0), Is.EqualTo (24.5));
+			Assert.That (y.Derivative (x1), Is.EqualTo (23));
+		}
+
+		private static Aad F(Aad x0, Aad x1)
+		{
+			return 2.0 * x0 * x0 - 3 * x0 / x1 + 5 * x0 * x1;
+		}
+	}
+}
+
