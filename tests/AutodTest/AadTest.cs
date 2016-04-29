f7d659bff85b20acbecf72beb6a32d8b3452f682
juna0613 <yuta.naito@gmail.com>
2016-04-29 20:55:52 +0900

    add tests

--- tests/AutodTest/AadTest.cs
+++ tests/AutodTest/AadTest.cs
@@ -17,6 +17,19 @@ namespace AutodTest
             Assert.That(y.Derivative(x1), Is.EqualTo(23));
         }
 
+        [Test]
+        public void AadTest2()
+        {
+            var x0 = new Aad(2.0);
+            var x1 = new Aad(3.0);
+            var x2 = new Aad(4.0);
+            var y = Aad.Exp(x0) * x1;
+            Assert.That(y.Value, Is.EqualTo(Math.Exp(2.0) * 3));
+            Assert.That(y.Derivative(x0), Is.EqualTo(Math.Exp(2.0) * 3));
+            Assert.That(y.Derivative(x1), Is.EqualTo(Math.Exp(2.0)));
+            Assert.That(y.Derivative(x2), Is.EqualTo(0.0));
+        }
+
         private static Aad F(Aad x0, Aad x1)
         {
             return 2.0 * x0 * x0 - 3 * x0 / x1 + 5 * x0 * x1;
